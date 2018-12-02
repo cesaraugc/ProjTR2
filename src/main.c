a8c768a43f1eac928ae8370cd0a967d0edea20bf
Cesar Carvalho <cesaraugc@gmail.com>
2018-12-02 00:08:16 -0200

    Adicionando funcionalidades

--- src/main.c
+++ src/main.c
@@ -8,8 +8,9 @@ int main(int argc, char *argv[])
   struct sockaddr_in *dest; /* socket info about the machine connecting to us */
   char rmsg[MAXRCVLEN];
   socklen_t socksize = sizeof(struct sockaddr_in);
-
+  FILE* fd;
   int ourSocket = createNewSocket(PORTNUM, 1);
+
   if ((dest = (struct sockaddr_in*)malloc(sizeof(struct sockaddr_in))) == NULL) {
     printf("Erro de alocacao. Abortando\n");
     freeMemory();
@@ -18,28 +19,23 @@ int main(int argc, char *argv[])
   freeMemoryList.dest = dest;
   int consocket = accept(ourSocket, (struct sockaddr *)dest, &socksize);
 
-  int len;
+  int len, i = 5;
 
-  while(consocket)
+  while(--i)
   {
       printf("Incoming connection from %s\n", inet_ntoa(dest->sin_addr));
 
       // write(consocket, msg, strlen(msg));
       len = read(consocket, rmsg, MAXRCVLEN);
       rmsg[len] = '\0';
-      printf("%s\n", rmsg);
+      // printf("%s\n", rmsg);
+      fd = fopen("request.txt","w");
+      fprintf(fd, "%s", rmsg);
+      system("nano request.txt");
       getchar();
-      // if (strstr(rmsg, "GET") == NULL) {// returns Bad request
-      //   printf("Bad request: \n");
-      //   printf("%s\n", rmsg);
-      //   char *somemsg;
-      //   if(strstr(rmsg, "HTTP/1.1"))
-      //     somemsg = "HTTP/1.1 400 BAD REQUEST\r\n\r\n";
-      //   else
-      //     somemsg = "HTTP/1.0 400 BAD REQUEST\r\n\r\n";
-      //   write(consocket, somemsg, strlen(somemsg));
-      // } else {
-      // }
+
+      makeRequest(fd);
+      fclose(fd);
       close(consocket);
       consocket = accept(ourSocket, (struct sockaddr *)dest, &socksize);
   }
---
+++ src/request.c
@@ -0,0 +1,41 @@
+#include "connection.h"
+
+// Funcao que realiza requisicao com um arquivo 
+void makeRequest(FILE* fd) {
+    struct sockaddr_in dest;
+    FILE* fd1;
+    struct hostent *hp;
+    char msg[500], buffer[500 + 1];
+    char *buff;
+    int someSocket, len;
+
+    if((hp = gethostbyname("www.df.gov.br")) == NULL){
+        fprintf(stderr, "Can't get server's address\n");
+        freeMemory();
+        exit(1);
+    }
+
+    while((buff = fgets(msg, 500, fd)) != NULL) {
+        snprintf(msg, strlen(buff), "%s%s", buff, "\r\n");
+    }
+    puts(msg);
+    printf("\nProceed?\n");
+    getchar();
+    someSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    memset(&dest, 0, sizeof(dest));                   /* zero the struct */
+    dest.sin_family = AF_INET;
+    dest.sin_addr.s_addr = inet_addr(hp->h_name);
+    // dest.sin_addr.s_addr = htonl(INADDR_LOOPBACK); /* set destination IP number - localhost, 127.0.0.1 */
+    dest.sin_port = htons(80);                        /* set destination port number */
+
+    connect(someSocket, (struct sockaddr *)&dest, sizeof(struct sockaddr_in));
+
+    write(someSocket, msg, strlen(msg));
+    fd1 = fopen("response.txt", "w");
+    while((len = read(someSocket, buffer, 500) >= 0)){
+        buffer[len] = '\0';
+        fprintf(fd1, "%s", buffer);        
+    }
+    fclose(fd1);
+}
